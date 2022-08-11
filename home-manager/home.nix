2b7d2bf54e6c08800d1173855c004d9026da4409
ISnortPennies <isnortpennies@protonmail.com>
2022-08-10 22:01:28 -0400

    switched to librewolf

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,7 +1,7 @@
 {config, pkgs, ...}:
 {
   imports = [
-    ./firefox.nix
+    ./librewolf.nix
     ./bspwm.nix
     ./sxhkd.nix
     ./rofi.nix
---
+++ home-manager/librewolf.nix
@@ -0,0 +1,19 @@
+{pkgs, ...}:{
+  programs = {
+    librewolf = {
+      enable = true;
+      settings = {
+        "webgl.disabled" = true;
+        "media.peerconnection.ice.no_host" = false;
+        "identity.fxaccounts.enabled" = true;
+        "browser.sessionstore.resume_from_crash" = false;
+        "security.OCSP.require" = false;
+        "network.dns.disableIPv6" = true;
+        "privacy.resistFingerprinting" = true;
+        "privacy.resistFingerprinting.letterboxing" = true;
+        "privacy.clearOnShutdown.history" = true;
+        "privacy.clearOnShutdown.downloads" = true;
+      };
+    };
+  };
+}
