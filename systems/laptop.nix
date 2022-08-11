4421b509581f001155423d213bd26a2c0dc44e7f
Greg Leyda <isnortpennies@protonmail.com>
2022-08-11 00:17:32 -0400

    refactored laptop

--- systems/laptop.nix
+++ systems/laptop.nix
@@ -29,5 +29,19 @@
       extraGroups = [ "wheel" "audio" "networkmanager"];
     };
   };
+  boot = {
+    initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" ];
+    kernelModules = [ "kvm-amd" ];
+  };
+  fileSystems = {
+    "/" = { 
+      device = "/dev/disk/by-uuid/c67796b3-d502-47db-8d0e-48f30bc91041";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/AD02-10EA";
+      fsType = "vfat";
+    };
+  };
 }
 
