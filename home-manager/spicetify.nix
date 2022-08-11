7ce7d951f79b4337e3c3ea537b79fb07a502fa38
Greg Leyda <isnortpennies@protonmail.com>
2022-08-11 00:36:56 -0400

    simplified spicetify

--- home-manager/spicetify.nix
+++ home-manager/spicetify.nix
@@ -1,11 +1,4 @@
 { pkgs, lib, spicetify-nix, ... }:
-let
-    spotifyNoPremiumSrc = pkgs.fetchgit {
-    url = "https://github.com/Daksh777/SpotifyNoPremium";
-    rev = "a2daa7a9ec3e21ebba3c6ab0ad1eb5bd8e51a3ca";
-    sha256 = "1sr6pjaygxxx6majmk5zg8967jry53z6xd6zc31ns2g4r5sy4k8d";
-  };
-in
 {
   imports = [ spicetify-nix.homeManagerModule ];
   programs.spicetify = {
@@ -16,20 +9,6 @@ in
       "adblock.js"
       "hidePodcasts.js"
     ];
-    theme = {
-      name = "spotifyNoPremium";
-      src = spotifyNoPremiumSrc;
-      appendName = false;
-      injectCss = true;
-      replaceColors = false;
-      overwriteAssets = false;
-      sidebarConfig = false;
-      requiredExtensions = [
-        {
-          src = spotifyNoPremiumSrc;
-          filename = "adblock.js";
-        }
-      ];
-    };
+    theme = "SpotifyNoPremium";
   };
 }
