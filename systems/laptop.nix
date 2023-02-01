41b40e570b6132793fcfa125160dcc37a97b57c7
ISnortPennies <ISnortPennies@protonmail.com>
2023-02-01 17:44:52 -0500

    changed laptop filesystem from uuid to device

--- systems/laptop.nix
+++ systems/laptop.nix
@@ -40,11 +40,11 @@
   };
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/c67796b3-d502-47db-8d0e-48f30bc91041";
+      device = "/dev/nvme0n1p2";
       fsType = "ext4";
     };
     "/boot" = {
-      device = "/dev/disk/by-uuid/AD02-10EA";
+      device = "/dev/nvme0n1p1";
       fsType = "vfat";
     };
   };
