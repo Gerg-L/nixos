a4cda1687c781cb6833b9bef7fa42dd50579d040
ISnortPennies <ISnortPennies@protonmail.com>
2023-02-01 21:13:25 -0500

    configured prime offloading correctly

--- modules/prime.nix
+++ modules/prime.nix
@@ -1,15 +1,30 @@
-{
+{pkgs, ...}: {
   hardware.nvidia = {
     prime = {
-      sync.enable = true;
+      offload.enable = true;
       amdgpuBusId = "PCI:5:0:0";
       nvidiaBusId = "PCI:1:0:0";
     };
-    nvidiaPersistenced = false;
+    powerManagement = {
+      enable = true;
+      finegrained = true;
+    };
+    nvidiaPersistenced = true;
     nvidiaSettings = false;
     modesetting.enable = true;
   };
   services.xserver = {
-    videoDrivers = ["modesetting" "nvidia"];
+    videoDrivers = ["nvidia"];
+    #disable DPMS
+    monitorSection = ''
+      Option "DPMS" "false"
+    '';
+    #disable screen blanking in total
+    serverFlagsSection = ''
+      Option "StandbyTime" "0"
+      Option "SuspendTime" "0"
+      Option "OffTime" "0"
+      Option "BlankTime" "0"
+    '';
   };
 }
