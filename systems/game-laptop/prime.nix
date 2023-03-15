478a7757aadcef6986996c5a3edae2641af5179f
Gerg-L <GregLeyda@proton.me>
2023-03-14 20:58:30 -0400

    added obs vlc and prime-run to laptop

--- systems/game-laptop/prime.nix
+++ systems/game-laptop/prime.nix
@@ -1,4 +1,17 @@
-_: {config, ...}: {
+_: {
+  config,
+  pkgs,
+  ...
+}: let
+  prime-run = pkgs.writeShellScriptBin "prime-run" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '';
+in {
+  environment.systemPackages = [prime-run];
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.latest;
     prime = {
