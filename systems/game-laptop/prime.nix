e9cb23e5e3a80c4a15d3f8ae7dba9c39224cd171
Gerg-L <GregLeyda@proton.me>
2023-03-18 22:42:24 -0400

    using primeCMD option not on search.nixos.org??

--- systems/game-laptop/prime.nix
+++ systems/game-laptop/prime.nix
@@ -2,20 +2,15 @@ _: {
   config,
   pkgs,
   ...
-}: let
-  prime-run = pkgs.writeShellScriptBin "prime-run" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec "$@"
-  '';
-in {
-  environment.systemPackages = [prime-run];
+}: {
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.latest;
     prime = {
-      offload.enable = true;
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      #      sync.enable = true;
       amdgpuBusId = "PCI:5:0:0";
       nvidiaBusId = "PCI:1:0:0";
     };
