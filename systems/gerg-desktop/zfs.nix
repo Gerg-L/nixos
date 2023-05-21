e1d4cb64c29d8d7e296b40206cacee0464fd8ee4
Gerg-L <GregLeyda@proton.me>
2023-05-21 18:01:30 -0400

    merged zfs.nix and erase-your-darlings.nix

--- systems/gerg-desktop/zfs.nix
+++ systems/gerg-desktop/zfs.nix
@@ -1,15 +1,41 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
+  #link some stuff
+  systemd.tmpfiles.rules = [
+    "L+ /etc/ssh/ssh_host_ed25519_key  - - - - /persist/ssh/ssh_host_ed25519_key"
+    "L+ /etc/ssh/ssh_host_ed25519_key.pub  - - - - /persist/ssh/ssh_host_ed25519_key.pub"
+    "L+ /etc/nixos - - - - /persist/nixos"
+  ];
+  #create machine-id for spotify
+  environment.etc = {
+    "machine-id".text = "b6431c2851094770b614a9cfa78fb6ea";
+  };
+  #make sure the sopskey is found
+  sops.age.sshKeyPaths = lib.mkForce ["/persist/ssh/ssh_host_ed25519_key"];
+  fileSystems."/persist".neededForBoot = true;
+
   boot = {
     zfs = {
       devNodes = "/dev/disk/by-id/";
       forceImportAll = true;
     };
     kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+    #disable hibernate and set cache max
     kernelParams = ["nohibernate" "zfs.zfs_arc_max=17179869184"];
     supportedFilesystems = ["zfs" "vfat"];
     initrd = {
+      #module for multiple swap devices
       kernelModules = ["dm_mod"];
+      #keyboard module for zfs password
       availableKernelModules = ["hid_generic"];
+      #wipe / and /var on boot
+      postDeviceCommands = lib.mkAfter ''
+        zfs rollback -r rpool/root@empty
+        zfs rollback -r rpool/var@empty
+      '';
     };
     plymouth.enable = false;
     loader = {
