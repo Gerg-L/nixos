2f04dc0e23c82a5bbcfb4d695f4a2d24b55dcd77
Gerg-L <GregLeyda@proton.me>
2023-05-21 18:24:37 -0400

    cleaned up packages.nix

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,14 +1,15 @@
-{pkgs, ...}: {
-  environment = {
-    defaultPackages = []; #don't install anything by default
-    systemPackages = [
-      pkgs.efibootmgr #efi editor
-      pkgs.pciutils #lspci
-      pkgs.alsa-utils #volume control
-      pkgs.xclip #commandline clipboard access
-      pkgs.bottom #view tasks
-      pkgs.nix-tree #view packages
-      pkgs.nix-output-monitor
-    ];
-  };
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  environment.defaultPackages = lib.mkForce [
+    pkgs.efibootmgr #efi editor
+    pkgs.pciutils #lspci
+    pkgs.alsa-utils #volume control
+    pkgs.xclip #commandline clipboard access
+    pkgs.bottom #view tasks
+    pkgs.nix-tree #view packages
+    pkgs.nix-output-monitor #nom nom nom nom];
+  ];
 }
