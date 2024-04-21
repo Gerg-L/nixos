3f0c37fcb7eb1df6dc88241ef535e063025bf1fe
Gerg-L <GregLeyda@proton.me>
2024-04-21 15:01:57 -0400

    re-enable parrot and move settings file from /data/settings

--- hosts/gerg-desktop/services/parrot.nix
+++ hosts/gerg-desktop/services/parrot.nix
@@ -7,12 +7,16 @@ _:
 }:
 {
   sops.secrets.discordenv = { };
+
   systemd.services.parrot = {
-    #wantedBy = [ "multi-user.target" ];
-    #wants = [ "network-online.target" ];
-    #after = [ "network-online.target" ];
-    script = lib.getExe pkgs.parrot;
+    wantedBy = [ "multi-user.target" ];
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+
+    environment.SETTINGS_PATH = "/persist/services/parrot";
+
     serviceConfig = {
+      ExecStart = lib.getExe pkgs.parrot;
       EnvironmentFile = config.sops.secrets.discordenv.path;
       Restart = "on-failure";
       RestartSec = "30s";
