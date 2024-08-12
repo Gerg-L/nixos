629d459b0886b800f5a55e990a0728c1c4671837
Gerg-L <GregLeyda@proton.me>
2024-08-11 22:47:58 -0400

    remove unused janitor package

--- packages/nix-janitor/package.nix
+++
@@ -1,9 +0,0 @@
-{ inputs', fetchFromGitHub }:
-inputs'.master.legacyPackages.nix-janitor.overrideAttrs {
-  src = fetchFromGitHub {
-    owner = "NobbZ";
-    repo = "nix-janitor";
-    rev = "d49fd7486d5597d2e854154bf643e7f86c5f1f6c";
-    hash = "sha256-KRXz2qUDWmyglXk56jL0twOQ3pWdpacddyVn95W7wl0=";
-  };
-}
