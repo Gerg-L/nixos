239a1645c6a1fb60d88522c74a59c206444a8f19
Gerg-L <GregLeyda@proton.me>
2025-04-28 17:57:41 -0400

    chore: update lavalinkPlugins

--- packages/lavalinkPlugins/package.nix
+++ packages/lavalinkPlugins/package.nix
@@ -4,10 +4,10 @@
 }:
 linkFarm "lavalinkPlugins" [
   {
-    name = "youtube-plugin-1.12.0.jar";
+    name = "youtube-plugin-1.13.0.jar";
     path = fetchurl {
-      url = "https://github.com/lavalink-devs/youtube-source/releases/download/1.12.0/youtube-plugin-1.12.0.jar";
-      hash = "sha256-M9CDMDGR7fBldPLLG/I5TN21DLNN440nzF1l34MrQK0=";
+      url = "https://github.com/lavalink-devs/youtube-source/releases/download/1.13.0/youtube-plugin-1.13.0.jar";
+      hash = "sha256-DL5s+a7HJel9Ouh9tJwFmCN25ThmIJNUhmQ7pn+moCg=";
     };
   }
 ]
