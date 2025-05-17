5b991350c7608d68e5b0ba1ef503084631092af3
Gerg-L <GregLeyda@proton.me>
2025-05-17 10:54:12 -0400

    fix: remove boot.initrd.systemd.strip

--- nixosConfigurations/gerg-desktop/kernel.nix
+++ nixosConfigurations/gerg-desktop/kernel.nix
@@ -5,40 +5,35 @@
 }:
 {
 
-  boot = {
-    # For linuxManualConfig to work: https://github.com/NixOS/nixpkgs/issues/368249
-    initrd.systemd.strip = false;
-
-    kernelPackages = pkgs.linuxPackagesFor (
-      let
-        version = "6.14.5";
-        src = pkgs.fetchurl {
-          url = "mirror://kernel/linux/kernel/v${builtins.head (lib.splitVersion version)}.x/linux-${version}.tar.xz";
-          hash = "sha256-KCB+xSu+qjUHAQrv+UT0QvfZ8isoa3nK9F7G3xsk9Ak=";
+  boot.kernelPackages = pkgs.linuxPackagesFor (
+    let
+      version = "6.14.5";
+      src = pkgs.fetchurl {
+        url = "mirror://kernel/linux/kernel/v${builtins.head (lib.splitVersion version)}.x/linux-${version}.tar.xz";
+        hash = "sha256-KCB+xSu+qjUHAQrv+UT0QvfZ8isoa3nK9F7G3xsk9Ak=";
+      };
+    in
+    (pkgs.linuxManualConfig {
+      inherit src;
+      inherit (config.boot) kernelPatches;
+      version = "${version}-gerg";
+      config = {
+        CONFIG_RUST = "y";
+        CONFIG_MODULES = "y";
+      };
+      configfile = ./kernelConfig;
+    }).overrideAttrs
+      (old: {
+        passthru = old.passthru or { } // {
+          features = lib.foldr (x: y: x.features or { } // y) {
+            efiBootStub = true;
+            netfilterRPFilter = true;
+            ia32Emulation = true;
+          } config.boot.kernelPatches;
         };
-      in
-      (pkgs.linuxManualConfig {
-        inherit src;
-        inherit (config.boot) kernelPatches;
-        version = "${version}-gerg";
-        config = {
-          CONFIG_RUST = "y";
-          CONFIG_MODULES = "y";
+        meta = old.meta or { } // {
+          broken = false;
         };
-        configfile = ./kernelConfig;
-      }).overrideAttrs
-        (old: {
-          passthru = old.passthru or { } // {
-            features = lib.foldr (x: y: x.features or { } // y) {
-              efiBootStub = true;
-              netfilterRPFilter = true;
-              ia32Emulation = true;
-            } config.boot.kernelPatches;
-          };
-          meta = old.meta or { } // {
-            broken = false;
-          };
-        })
-    );
-  };
+      })
+  );
 }
