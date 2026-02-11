73cfab111e26ee7ccc654c21e20bb331306ef3df
Gerg-L <GregLeyda@proton.me>
2026-02-11 18:42:46 -0500

    refactor: collapse polkit rules

--- nixosModules/security.nix
+++ nixosModules/security.nix
@@ -11,13 +11,8 @@
       enable = true;
       extraConfig = ''
         polkit.addRule(function(action, subject) {
-          if (action.id == "org.freedesktop.policykit.exec") {
-            return polkit.Result.AUTH_ADMIN_KEEP;
-          }
-        });
-
-        polkit.addRule(function(action, subject) {
-          if (action.id.indexOf("org.freedesktop.systemd1.") == 0) {
+          if (action.id == "org.freedesktop.policykit.exec"
+              || action.id.indexOf("org.freedesktop.systemd1.") == 0) {
             return polkit.Result.AUTH_ADMIN_KEEP;
           }
         });
