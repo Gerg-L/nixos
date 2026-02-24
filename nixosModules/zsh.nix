8947b322db5cb1f55c9b8f7ee605f7b34cc15aac
Gerg-L <GregLeyda@proton.me>
2026-02-23 22:01:17 -0500

    feat: banish zsh-vi-mode into the shadow realm

--- nixosModules/zsh.nix
+++ nixosModules/zsh.nix
@@ -9,42 +9,41 @@
       histSize = 20000;
       histFile = "$HOME/.cache/zsh_history";
       interactiveShellInit = ''
-          ### zsh-history-substring-search ###
-          setopt HIST_IGNORE_ALL_DUPS
-          source ${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh
-          bindkey '^[[A' history-substring-search-up
-          bindkey '^[[B' history-substring-search-down
-          ### fzf-tab ###
-          source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
-          ### vi-mode ###
-          source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
-          ### transient shell prompt ###
-          zle-line-init() {
-          emulate -L zsh
+        ### zsh-history-substring-search ###
+        setopt HIST_IGNORE_ALL_DUPS
+        source ${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+        bindkey '^[[A' history-substring-search-up
+        bindkey '^[[B' history-substring-search-down
 
-          [[ $CONTEXT == start ]] || return 0
+        ### fzf-tab ###
+        source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
 
-          while true; do
-            zle .recursive-edit
-            local -i ret=$?
-           [[ $ret == 0 && $KEYS == $'\4' ]] || break
-           [[ -o ignore_eof ]] || exit 0
-          done
+        zle-line-init() {
+        emulate -L zsh
 
-         local saved_prompt=$PROMPT
-          local saved_rprompt=$RPROMPT
-          PROMPT='\$ '
-          RPROMPT='''
-          zle .reset-prompt
-          PROMPT=$saved_prompt
-          RPROMPT=$saved_rprompt
+        [[ $CONTEXT == start ]] || return 0
 
-          if (( ret )); then
-            zle .send-break
-          else
-            zle .accept-line
-          fi
-          return ret
+        while true; do
+          zle .recursive-edit
+          local -i ret=$?
+         [[ $ret == 0 && $KEYS == $'\4' ]] || break
+         [[ -o ignore_eof ]] || exit 0
+        done
+
+        local saved_prompt=$PROMPT
+        local saved_rprompt=$RPROMPT
+        PROMPT='\$ '
+        RPROMPT='''
+        zle .reset-prompt
+        PROMPT=$saved_prompt
+        RPROMPT=$saved_rprompt
+
+        if (( ret )); then
+          zle .send-break
+        else
+          zle .accept-line
+        fi
+        return ret
         }
 
         zle -N zle-line-init
