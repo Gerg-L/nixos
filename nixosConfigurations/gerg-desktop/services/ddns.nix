273b2c18ea2a0e9bbaa9dbeaadc52cd75efab5d4
Gerg-L <GregLeyda@proton.me>
2026-03-22 17:50:43 -0400

    refactor: use ddns-updater instead of my own script

--- nixosConfigurations/gerg-desktop/services/ddns.nix
+++ nixosConfigurations/gerg-desktop/services/ddns.nix
@@ -1,30 +1,52 @@
 {
   config,
-  pkgs,
-  _dir,
 }:
 {
-  sops.secrets.cloudflare = { };
-
-  systemd.services.ddns = {
-    reloadIfChanged = false;
-    restartIfChanged = false;
-    stopIfChanged = false;
-    wants = [ "network-online.target" ];
-    after = [ "network-online.target" ];
-    startAt = "hourly";
-
-    serviceConfig = {
-      EnvironmentFile = config.sops.secrets.cloudflare.path;
-      DynamicUser = true;
+  sops = {
+    secrets.cloudflare = { };
+    templates.ddns = {
+      restartUnits = [ "ddns-updater.service" ];
+      content = builtins.toJSON {
+        settings =
+          map
+            (
+              x:
+              {
+                provider = "cloudflare";
+                token = config.sops.placeholder.cloudflare;
+                ttl = 1;
+                zone_identifier = "8f76f071c5edbc0f947a5c5f9c5df9f8";
+                ip_version = "ipv6";
+                ipv6_suffix = "0:0:0:0:da5e:d3ff:fee5:4790/64";
+              }
+              // x
+            )
+            [
+              {
+                domain = "*.gerg-l.com";
+                proxied = true;
+              }
+              {
+                domain = "gerg-l.com";
+                proxied = false;
+              }
+              {
+                domain = "minecraft.gerg-l.com";
+                proxied = false;
+              }
+            ];
+      };
     };
-
-    path = [
-      pkgs.netcat
-      pkgs.jq
-      pkgs.curl
-    ];
-
-    script = builtins.readFile "${_dir}/ddns_script.sh";
   };
+
+  services.ddns-updater = {
+    enable = true;
+    environment = {
+      CONFIG_FILEPATH = "%d/config.json";
+      PERIOD = "1h";
+      SERVER_ENABLED = "no";
+    };
+  };
+  systemd.services.ddns-updater.serviceConfig.LoadCredential =
+    "config.json:${config.sops.templates.ddns.path}";
 }
