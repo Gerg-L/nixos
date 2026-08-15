9f1660ff3607cad0da2333150413b26f667ce5b2
Gerg-L <GregLeyda@proton.me>
2026-08-14 20:45:50 -0400

    fix: flat-remix died

--- nixosModules/theming.nix
+++ nixosModules/theming.nix
@@ -15,8 +15,6 @@ in
     (lib.mkIf cfg.enable {
       environment = {
         systemPackages = [
-          pkgs.flat-remix-gtk
-          pkgs.flat-remix-icon-theme
           pkgs.quintom-cursor-theme
         ];
         etc = {
@@ -25,23 +23,17 @@ in
             gtk-cursor-theme-name=Quintom_Ink
             gtk-cursor-theme-size=16
             gtk-font-name=Overpass 10
-            gtk-icon-theme-name=Flat-Remix-Blue-Dark
-            gtk-theme-name=Flat-Remix-GTK-Blue-Darkest
           '';
           "xdg/gtk-3.0/settings.ini".text = ''
             [Settings]
             gtk-cursor-theme-name=Quintom_Ink
             gtk-cursor-theme-size=16
             gtk-font-name=Overpass 10
-            gtk-icon-theme-name=Flat-Remix-Blue-Dark
-            gtk-theme-name=Flat-Remix-GTK-Blue-Darkest
           '';
           "xdg/gtk-2.0/gtkrc".text = ''
             gtk-cursor-theme-name = "Quintom_Ink"
             gtk-cursor-theme-size = 16
             gtk-font-name = "Overpass 10"
-            gtk-icon-theme-name = "Flat-Remix-Blue-Dark"
-            gtk-theme-name = "Flat-Remix-GTK-Blue-Darkest"
           '';
           "xdg/Xresources".text = ''
             Xcursor.size: 16
